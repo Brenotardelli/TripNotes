cef5b546dbaa9e52226c13c3f229ebc3b09e9da4
Breno Tardelli <brenot.dev@gmail.com>
2025-05-15 16:19:21 +0200

    navigate added to the app

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import styles from "./Map.module.css";
 import SideBar from "./SideBar";
+import { useNavigate, useSearchParams } from "react-router-dom";
 const Map = () => {
-  return <div className={styles.mapContainer}>Map</div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      <h1>Map</h1>
+      <h2>
+        Positiion: {lat}, {lng}
+      </h2>
+    </div>
+  );
 };
 
 export default Map;
